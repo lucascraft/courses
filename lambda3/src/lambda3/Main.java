6954ae61739e048271eb4e4c8992b5b2497745b0
machi <machi@H3-R0N>
2018-01-21 20:23:11 +0100

    simplify

--- lambda3/src/lambda3/Main.java
+++ lambda3/src/lambda3/Main.java
@@ -4,7 +4,6 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import lambda3.conn.ConnectionPool;
-import lambda3.conn.IConnectionPool;
 import lambda3.garage.Garage;
 
 public class Main 
@@ -16,10 +15,10 @@ public class Main
 		Garage garage = injector.getInstance(Garage.class);
 	    garage.init("in/vehicles.json");
 	   
-	    IConnectionPool pool = injector.getInstance(IConnectionPool.class);
+	    ConnectionPool pool = injector.getInstance(ConnectionPool.class);
 	    pool.init("in/connections.json");
 
-	    ((ConnectionPool)pool).getConnectionsMap().values().forEach(c -> {
+	    pool.getConnectionsMap().values().forEach(c -> {
 	    	System.out.println(c.getConnector().getURL());
 	    });
 	    
