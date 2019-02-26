3b4d7fa4526596ef0162609632ca342186f509dc
lucas <lucas@LAPTOP-2SKH5AVA>
2019-02-26 10:02:10 +0100

    enhance TUs

--- pythonCourse1/learn/it/VUtils.py
+++ pythonCourse1/learn/it/VUtils.py
@@ -1,17 +1,40 @@
 from builtins import classmethod
  
+#
+# Visual utils
+#
+
 class VUtils(object):
 
     from colormath.color_objects import LabColor
 
+    #
+    # default color
+    #
     defaultlab = LabColor(0.903, 16.296, -2.22)
     
+    #
+    # color registry
+    #
     colorRegistry = dict()
     
+    #
+    # constructor
+    #
     def __init__(self):
         return
-    
+        
     @classmethod
+    #
+    # register color
+    #
+    def register_Color(self, k):
+        self.get_Color(k)
+
+    @classmethod
+    #
+    # get color
+    #
     def get_Color(self, k):
         if str(k).startswith("#"):
             
@@ -23,21 +46,32 @@ class VUtils(object):
             fgg = 1.0 * int(gg, 16)                   
             fbb = 1.0 * int(bb, 16)                  
             
-            self.colorRegistry[str(k)] = self.LabColor(frr, fgg, fbb)
+            if self.colorRegistry.get(str(k)) is None:
+                self.colorRegistry[str(k)] = self.LabColor(frr, fgg, fbb)
+                print("Added color " + str(k))
             
             return self.colorRegistry[k]
         else:
             return self.defaultlab
 
     @classmethod
+    #
+    # get keys
+    #
     def getKeys(self):
         return self.colorRegistry.keys()
 
     @classmethod
+    #
+    # get colors
+    #
     def getColors(self):
         return self.colorRegistry.values()
 
     @classmethod
+    #
+    # get colors registry
+    #
     def getColorsRegistry(self):
         return self.colorRegistry
             
--- pythonCourse1/learn/tests/VUtilsTest.py
+++ pythonCourse1/learn/tests/VUtilsTest.py
@@ -3,21 +3,49 @@ import unittest
 from learn.it.VUtils import VUtils
 from test.test_deque import fail
 
+#
+# Test utils
+#
+
 class VUtilTest(unittest.TestCase):
 
+    #
+    # setup
+    #
+    
     def setUp(self):
-        self.color =  VUtils.get_Color("#AABBCC");
+        self.colors = ["#AABBCC", "#FF00AA", "#123456", "#EE2312", "#AACCFF"]
+        for c in self.colors:
+            VUtils.register_Color(c)
         pass
 
 
+    #
+    # tear down
+    #
+    
     def tearDown(self):
         del self.color
         pass
 
+    #
+    # test color registry
+    #
+    
+    def testColorRegistryInit(self):
+        print("Test registry")
+        if len(VUtils.getColorsRegistry().keys()) != 5:
+            fail("Color registry length should have been 5")
+        else:
+            pass
+    
+    #
+    # test color
+    #
+    
     def testColorFromRGBHex(self):
-        
-        print("Test #AABBCC")
-        
+        self.color =  VUtils.get_Color("#AABBCC");
+        print("Test #AABBCC exist")
         if int(self.color.get_value_tuple()[0]) < 0 or int(self.color.get_value_tuple()[0]) > 255:
             fail("red value should have been on range [0-255] and is " + str(self.color.get_value_tuple()[0]))
         if int(self.color.get_value_tuple()[1]) < 0 or int(self.color.get_value_tuple()[1]) > 255:
@@ -26,7 +54,6 @@ class VUtilTest(unittest.TestCase):
             fail("blue value should have been on range [0-255] and is " + str(self.color.get_value_tuple()[2]))
         else:
             pass
-            
 
 
 if __name__ == "__main__":
