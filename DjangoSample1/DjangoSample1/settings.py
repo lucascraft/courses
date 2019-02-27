ae302adea9b7d840b6d90252b14d296f40d3363f
lucas <lucas@LAPTOP-2SKH5AVA>
2019-02-27 11:52:23 +0100

    users/group views rest configurations

--- DjangoSample1/DjangoSample1/settings.py
+++ DjangoSample1/DjangoSample1/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'DjangoSample1.wsgi.application'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
